7e9d0e42dddc22555a1621578f42e91f5f52cbbd
lelooouch <118593450+lelooouch@users.noreply.github.com>
2025-12-12 12:51:41 +0500

    Create init.py

---
+++ Educational_Plagiarism_Detector/src/init.py
@@ -0,0 +1,14 @@
+"""
+Plagiarism Detection System for Educational Purposes
+"""
+
+__version__ = "1.0.0"
+__author__ = "Educational Project Team"
+
+from .main import PlagiarismDetector, AnalysisResults, SimilarityResult
+
+__all__ = [
+    "PlagiarismDetector",
+    "AnalysisResults", 
+    "SimilarityResult"
+]
